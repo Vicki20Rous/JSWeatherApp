e251c40e65e62b52b5df665b85831d5589f63a35
Victoria Rouser <vic86rous@gmail.com>
2022-03-11 17:34:23 -0600

    about to pull data in console log

--- script.js
+++ script.js
@@ -5,12 +5,18 @@ let weather = {
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q=" +
             city +
-            "&units=metric&appid=" +
+            "&units=imperial&appid=" +
             this.apiKey
         )
             .then((response) => response.json())
-            .then((data) => console.log(data));
+            .then((data) => this.displayWeather(data));
     },
 
-
+    displayWeather : function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed);
+    }
 };
